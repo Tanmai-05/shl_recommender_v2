934e05fe84a123fc3d843d52fb3edce08e6695a4
Tanmai-05 <tanmaimanda@gmail.com>
2025-04-09 18:53:37 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from .recommend import recommend_assessments
+from recommend import recommend_assessments
 
 # Define the FastAPI app
 app = FastAPI()
